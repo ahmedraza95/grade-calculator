a4c737141bb93aa2ba9ad17fadd230a64350187a
ahmedraza95 <ahmedhome6789@gmail.com>
2024-03-14 02:14:47 +0500

    changes

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ let result = (multiply / totalMarks);
 
 if (result > 80) {
     alert("congratulation you achieve A1 Grade and your percentage is" + " " + result);
-  } else {
+  }else if (result > 60) { 
     alert("nice effort you achieve B Grade and your percentage is" + " " + result);
+  }else if (result > 40) { 
+    alert("great achieve C Grade and your percentage is" + " " + result);
   }
-
+   else {
+    alert("oh my God You are Fail your grade is F your percentage is" + " " + result);
+  }
