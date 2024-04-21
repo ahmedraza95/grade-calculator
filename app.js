ac43ba727a20b70e40dc7b08ac760e3e36a3d51b
ahmedraza95 <ahmedhome6789@gmail.com>
2024-04-21 18:58:48 +0500

    third

--- app.js
+++ app.js
@@ -18,25 +18,32 @@ inputForm.addEventListener("submit", (event) => {
   let multiply = obtain * 100;
   let result = (multiply / total);
 
-  result = parseFloat(result)
 
   console.log(result);
 
   table.innerHTML = "";
 
-  if (result => 80) {
-    table.innerHTML += "<p>Congratulation you achieve A1-Grade and your percentage is " + result + "% 🥳🥳</p>" ;
+  // if (result => 80) {
+  //   table.innerHTML += "<p>Congratulation you achieve A1-Grade and your percentage is " + result + "% 🥳🥳</p>" ;
+  // } else if (weather < 32 || weather >= 28) {
+  //   table.innerHTML += "<p>Congratulation you achieve A1-Grade and your percentage is " + result + "% 🥳🥳</p>" ;
+  // } else {
+  //   table.innerHTML += "<p>oohhhh Your Fail  you achieve F-Grade and your percentage is " + result + "% 😭😭😭 Better luck For next time</p>" ;
+  // }
   
-  } else if (result => 70 ) {
-    table.innerHTML += "<p>Nice Effort you achieve A-Grade and your percentage is " + result + "% 🥳🥳</p>" ;
-  } else if (result => 65 ) {
-    table.innerHTML += "<p>GOOD you achieve B+-Grade and your percentage is " + result + "% 🥳🥳</p>" ;
-  }else if (result => 60 ) {
-    table.innerHTML += "<p>Nice Need more better you achieve B-Grade and your percentage is " + result + "% 🥳🥳</p>" ;
 
-  } else if (result => 50 ) {
+
+  if (result >= 80) {
+    table.innerHTML += "<p>Congratulation you achieve A1-Grade and your percentage is " + result + "% 🥳🥳</p>" ;
+  } else if (result >= 70) {
+    table.innerHTML += "<p>Congratulation you achieve A-Grade and your percentage is " + result + "% 🥳🥳</p>" ;
+  } else if (result >= 65 ) {
+    table.innerHTML += "<p>GOOD you achieve B+-Grade and your percentage is " + result + "% 🥳🥳</p>" ;
+  }else if (result >= 60 ) {
+    table.innerHTML += "<p>Nice Need more better you achieve B-Grade and your percentage is " + result + "% 🥳🥳</p>" ;
+  } else if (result >= 50 ) {
     table.innerHTML += "<p>Well Done you achieve c-Grade and your percentage is " + result + "% 🥳🥳</p>" ;
-  } else if (result => 40 ) {
+  } else if (result >= 40 ) {
     table.innerHTML += "<p>just passing marks need better for next time you achieve D-Grade and your percentage is " + result + "% 🥳🥳</p>" ;
   } else {
     table.innerHTML += "<p>oohhhh Your Fail  you achieve F-Grade and your percentage is " + result + "% 😭😭😭 Better luck For next time</p>" ;
